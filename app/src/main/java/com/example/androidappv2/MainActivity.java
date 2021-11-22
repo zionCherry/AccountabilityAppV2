a89c1c97b2cdd302e1577d2694649189cdab28b3
rollerr99 <90643144+rollerr99@users.noreply.github.com>
2021-11-22 18:14:10 -0500

    I am working on implementing a new activity page after the create group button is pressed. Here is the link to where I was working from this implemented. https://www.tutorialspoint.com/how-to-start-new-activity-on-click-button-in-android

--- app/src/main/java/com/example/androidappv2/MainActivity.java
+++ app/src/main/java/com/example/androidappv2/MainActivity.java
@@ -1,5 +1,5 @@
 package com.example.androidappv2;
-//nothing crazy just trying to see if this will commit
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -24,13 +24,22 @@ public class MainActivity extends AppCompatActivity {
         });
 
         Button btnCreate = (Button) findViewById(R.id.buttonCreate);
-        btnCreate.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Toast.makeText(getApplicationContext(), "Create Group Button is Clicked", Toast.LENGTH_SHORT)
-                        .show();
+            public void onClick(View v) {
+                openNewActivity();
             }
         });
+
+        //btnCreate.setOnClickListener(new View.OnClickListener() {
+           // @Override
+            //public void onClick(View view) {
+                //Toast.makeText(getApplicationContext(), "Create Group Button is Clicked", Toast.LENGTH_SHORT)
+                        //.show();
+            //}
+        //});
 
         Button btnView = (Button) findViewById(R.id.buttonView);
         btnView.setOnClickListener(new View.OnClickListener() {
@@ -40,5 +49,9 @@ public class MainActivity extends AppCompatActivity {
                         .show();
             }
         });
+    }
+    public void openNewActivity(){
+        Intent intent = new Intent(this, NewActivity.class);
+        startActivity(intent);
     }
     }
---
+++ app/src/main/java/com/example/androidappv2/MainActivity2.java
@@ -0,0 +1,11 @@
+package com.example.androidappv2;
+
+        import android.support.v8.app.AppCompatActivity;
+        import android.os.Bundle;
+public class NewActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new);
+    }
+}
