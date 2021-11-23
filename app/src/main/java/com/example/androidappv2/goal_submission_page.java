903ad3c6ba4d64a3945c87409eceb9c9ffd536a2
DESKTOP-DTHLF58\T Sam <tysamay@gmail.com>
2021-11-23 05:18:20 -0500

    UI for Goal Submission Page has been Established

---
+++ app/src/main/java/com/example/androidappv2/goal_submission_page.java
@@ -0,0 +1,14 @@
+package com.example.androidappv2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class goal_submission_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_goal_submission_page);
+    }
+}
