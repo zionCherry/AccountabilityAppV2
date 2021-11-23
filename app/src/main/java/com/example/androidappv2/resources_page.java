a34a0c9f2bcd7e28e91cf8e6a2a100264a975e49
DESKTOP-DTHLF58\T Sam <tysamay@gmail.com>
2021-11-23 05:51:49 -0500

    Establishing Resoures and Information UI

---
+++ app/src/main/java/com/example/androidappv2/resources_page.java
@@ -0,0 +1,14 @@
+package com.example.androidappv2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class resources_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resources_page);
+    }
+}
