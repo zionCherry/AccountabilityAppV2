9a7414893288a9cb202c18de00a3eb70a2690d2d
DESKTOP-DTHLF58\T Sam <tysamay@gmail.com>
2021-11-23 04:58:41 -0500

    ViewGroupButton now Pushes to New Activity View Team Button

--- app/src/main/java/com/example/androidappv2/view_button_page.java
+++ app/src/main/java/com/example/androidappv2/view_button_page.java
@@ -2,7 +2,10 @@ package com.example.androidappv2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class view_button_page extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class view_button_page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_button_page);
-    }
-}
+
+        Button btnView =  findViewById(R.id.buttonViewGroup);
+        btnView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openViewTeamPage();
+            }
+            private void openViewTeamPage()
+            {
+                Intent openViewTeam = new Intent(view_button_page.this,view_team_button.class);
+                startActivity(openViewTeam);
+            }
+        });}}
