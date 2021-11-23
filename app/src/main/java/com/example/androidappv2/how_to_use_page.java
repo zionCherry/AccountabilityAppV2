9a4e4757a9a07c647e46fa084bc00a39fd18af27
DESKTOP-DTHLF58\T Sam <tysamay@gmail.com>
2021-11-23 06:10:34 -0500

    How to Use and guide for point calculation Activity Page Created

---
+++ app/src/main/java/com/example/androidappv2/how_to_use_page.java
@@ -0,0 +1,14 @@
+package com.example.androidappv2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class how_to_use_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_how_to_use_page);
+    }
+}
