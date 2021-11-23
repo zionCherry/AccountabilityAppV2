87b6f3c920ee7b42e5244f540c3948d0c315fce1
mehmetkutlug <kutlugmk@vcu.edu>
2021-11-23 18:12:19 -0500

    Extended User Class
    Added functions to change password,
     added variable score, and added ways to adjust score

--- app/src/main/java/Users.java
+++ app/src/main/java/Users.java
@@ -1,19 +1,38 @@
 public class Users {
-    public class User{
-        String Username;
-        String Password;
+    String Username;
+    String Password;
+    int Score;
 
-        public User(String un, String pw){
-            Usernam = un;
-            Password = ow;
-        }
+    public void User(String un, String pw){
+        Usernam = un;
+        Password = ow;
+        score = 0;
+    }
+    public String getUsername(){
+        return Usernam;
+    }
 
-        public String getUsername(){
-            return Usernam;
-        }
+    public boolean checkPassword(String pw){
+        return pw == Password;
+    }
 
-        public boolean checkPassword(String pw){
-            return pw == Password;
-        }
+    public void changePassword(String pw){
+        Password = pw;
+    }
+
+    public int getScore() {
+        return Score;
+    }
+
+    public void addScore(int add){
+        Score = Score + add;
+    }
+
+    public void subScore(int sub){
+        Score = Score - sub;
+    }
+
+    public void setScore(int scr){
+        Score = scr;
     }
 }
