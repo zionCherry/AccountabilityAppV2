bbed03cea4d77c2899bcc523b276e5bd820f85fe
DESKTOP-DTHLF58\T Sam <tysamay@gmail.com>
2021-11-23 05:28:44 -0500

    Establishes Button from View Team to Submit Goal

--- app/src/main/java/com/example/androidappv2/view_team_button.java
+++ app/src/main/java/com/example/androidappv2/view_team_button.java
@@ -2,7 +2,10 @@ package com.example.androidappv2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class view_team_button extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class view_team_button extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_team_button);
-    }
-}
+
+        Button btnView =  findViewById(R.id.goalSubmissionButton);
+        btnView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openGoalSubmissionPage();
+            }
+            private void openGoalSubmissionPage()
+            {
+                Intent openViewTeam = new Intent(view_team_button.this,goal_submission_page.class);
+                startActivity(openViewTeam);
+            }
+        });}}
