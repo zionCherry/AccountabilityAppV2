552a14dcccf5d7efa1ee56c75de11b81c046de3d
rollerr99 <90643144+rollerr99@users.noreply.github.com>
2021-11-23 17:10:34 -0500

    code readability change to the find button section

--- app/src/main/java/com/example/androidappv2/find_button_page.java
+++ app/src/main/java/com/example/androidappv2/find_button_page.java
@@ -5,10 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class find_button_page extends AppCompatActivity {
-
+    //this is the function definition for the find button page
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_find_button_page);
+        super.onCreate(savedInstanceState);// creates the new state
+        setContentView(R.layout.activity_find_button_page);// is responsible for actually changing what is viewed
     }
 }
