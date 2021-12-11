1d81f33f653f4c3390e7a40ae84fb5f946141819
DESKTOP-QMQACAV\zionc <cherryzc@vcu.edu>
2021-12-11 17:34:21 -0500

    Working to create the Database given the users entered data

--- app/src/main/java/com/example/androidappv2/Create_Button_Page.java
+++ app/src/main/java/com/example/androidappv2/Create_Button_Page.java
@@ -3,13 +3,26 @@ package com.example.androidappv2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class Create_Button_Page extends AppCompatActivity {
     //this section runs the create button page extension
+    String groupName, groupObj;
+    int groupNum;
+
+    EditText editGroupName;
+    EditText editGroupNum;
+    EditText editGroupObj;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);// creates the instance for the for the state that will be viewed
         setContentView(R.layout.activity_create_button_page);//actually is responsible for changing what page is viewed
+
+         editGroupName = (EditText) findViewById(R.id.editGroupName);
+         editGroupNum = (EditText) findViewById(R.id.editGroupNum);
+         editGroupObj = (EditText) findViewById(R.id.editGroupObj);
+
     }
 }
 
