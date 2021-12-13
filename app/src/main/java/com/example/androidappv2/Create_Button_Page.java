cac27aeca2bcc681524a7c73a73520b1be341f1c
DESKTOP-QMQACAV\zionc <cherryzc@vcu.edu>
2021-12-13 11:38:12 -0500

    Working to create the Database given the users entered data Added an extra page to input user names

--- app/src/main/java/com/example/androidappv2/Create_Button_Page.java
+++ app/src/main/java/com/example/androidappv2/Create_Button_Page.java
@@ -2,6 +2,7 @@ package com.example.androidappv2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,6 +10,7 @@ import android.widget.EditText;
 
 public class Create_Button_Page extends AppCompatActivity {
     //this section runs the create button page extension
+    //This is the logic to obtain user data from the required fields in the app
     String groupName, groupObj;
     int groupNum;
 
@@ -23,11 +25,10 @@ public class Create_Button_Page extends AppCompatActivity {
         super.onCreate(savedInstanceState);// creates the instance for the for the state that will be viewed
         setContentView(R.layout.activity_create_button_page);//actually is responsible for changing what page is viewed
 
-         editGroupName = (EditText) findViewById(R.id.editGroupName);
-         editGroupNum = (EditText) findViewById(R.id.editGroupNum);
-         editGroupObj = (EditText) findViewById(R.id.editGroupObj);
-
-         submitButton = (Button) findViewById(R.id.subButton);
+         editGroupName = (EditText)  findViewById(R.id.editGroupName);
+         editGroupNum =  (EditText)  findViewById(R.id.editGroupNum);
+         editGroupObj =  (EditText)  findViewById(R.id.editGroupObj);
+         submitButton =  (Button)    findViewById(R.id.subButton);
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -35,6 +36,13 @@ public class Create_Button_Page extends AppCompatActivity {
                 groupNum = Integer.valueOf(editGroupNum.getText().toString());
                 groupObj = editGroupObj.getText().toString();
 
+                openEnterNamesPage(groupNum);
+            }
+
+            private void openEnterNamesPage(int groupNum)
+            {
+                Intent openGroupNames = new Intent(Create_Button_Page.this,enter_groupnames_page.class);
+                startActivity(openGroupNames);
             }
         });
     }
---
+++ app/src/main/java/com/example/androidappv2/enter_groupnames_page.java
@@ -0,0 +1,14 @@
+package com.example.androidappv2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class enter_groupnames_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enter_groupnames_page);
+    }
+}
