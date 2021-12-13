52e849a1cebcf638d08213e179c72bd5f42c6254
DESKTOP-DTHLF58\T Sam <tysamay@gmail.com>
2021-12-13 17:58:15 -0500

    Updated UI and Bug Fixed Small Errors within Code, Additional code added to check for Errors along with Addition of Android Google Play SDK Addition

--- app/src/main/java/com/example/androidappv2/goal_submission_page.java
+++ app/src/main/java/com/example/androidappv2/goal_submission_page.java
@@ -10,17 +10,13 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import java.util.Calendar;
 
 public class goal_submission_page extends AppCompatActivity {
 
     EditText title;
-    EditText startdate;
-    EditText enddate;
     EditText description;
-    EditText email;
     Button submitbutton;
-
+    EditText location;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,38 +24,38 @@ public class goal_submission_page extends AppCompatActivity {
         setContentView(R.layout.activity_goal_submission_page);
 
         title = findViewById(R.id.title);
-        startdate  = findViewById(R.id.start_event);
-        enddate = findViewById(R.id.end_event);
         description = findViewById(R.id.description);
+        location = findViewById(R.id.locationtext);
         submitbutton = findViewById(R.id.submitbutton);
 
         submitbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (!title.getText().toString().isEmpty()
-                    && !description.getText().toString().isEmpty()
-                        && !email.getText().toString().isEmpty()) {
+                    && !description.getText().toString().isEmpty() &&!location.getText().toString().isEmpty()) {
 
                     Intent intent = new Intent(Intent.ACTION_INSERT);
                     intent.setData(CalendarContract.Events.CONTENT_URI);
 
                     intent.putExtra(CalendarContract.Events.TITLE, title.getText().toString());
                     intent.putExtra(CalendarContract.Events.DESCRIPTION, description.getText().toString());
-                    intent.putExtra(CalendarContract.Events.ALL_DAY,true);
-                    intent.putExtra(Intent.EXTRA_EMAIL,"test@google.com, test@goodgle.com, test@google.com");
+                    intent.putExtra(CalendarContract.Events.EVENT_LOCATION, location.getText().toString());
+                    intent.putExtra(CalendarContract.Events.ALL_DAY, true);
+                    intent.putExtra(Intent.EXTRA_EMAIL, "test@google.com");
 
-                    if(intent.resolveActivity(getPackageManager()) !=null){
+                    if (intent.resolveActivity(getPackageManager()) != null) {
                         startActivity(intent);
-                    } else
-                    {
+                    } else {
+                        Toast.makeText(goal_submission_page.this, "there is no app that can support this action",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                }else{
                         Toast.makeText(goal_submission_page.this,"Please fill all the fields",
                             Toast.LENGTH_SHORT).show();
-
                     }
 
 
                 }
-            }
-        });
+            });
         };
-    }
+        }
