e0756648f646179acff72d97cc0bbd38e9798978
rollerr99 <90643144+rollerr99@users.noreply.github.com>
2021-12-15 19:01:14 -0500

    added a button to go back to the home page from the user profile page

--- app/src/main/java/com/example/androidappv2/user_profile_Page.java
+++ app/src/main/java/com/example/androidappv2/user_profile_Page.java
@@ -1,6 +1,9 @@
 package com.example.androidappv2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +12,21 @@ public class user_profile_Page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);// creates the next state
         setContentView(R.layout.activity_user_profile_page); // actually changes what is viewed to the resource page state
+
+        //this section handles connecting the user profile to the home page
+        Button btnHome = findViewById(R.id.buttonHome);
+        btnHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openHomePage();
+            }
+            private void openHomePage()
+            {
+                Intent intent = new Intent(user_profile_Page.this, MainActivity.class);
+                startActivity(intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+            }
+        });
+
+
     }
 }
